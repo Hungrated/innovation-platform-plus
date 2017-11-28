beb6dda970f7e63e278f2e627b2594e14c7b9c3b
hungrated <zhang295415658@qq.com>
2017-11-28 17:06:54 +0800

    modify styles & modify ajax module

--- src/client/main.js
+++ src/client/main.js
@@ -4,21 +4,21 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 import Routers from './routers';
-import VueResource from 'vue-resource';
+// import axios from 'axios';
 import App from './App';
 import iView from 'iview';
 import 'iview/dist/styles/iview.css';
 
 Vue.use(iView);
 Vue.use(VueRouter);
-Vue.use(VueResource);
-Vue.config.productionTip = false;
+// Vue.use(axios);
 
 const router = new VueRouter({
   mode: 'history',
   routes: Routers
 });
 
+/* eslint-disable no-unused-vars */
 let vm = new Vue({
   el: '#app',
   router: router,
