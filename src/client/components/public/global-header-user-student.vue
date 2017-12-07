ddddc66bf3a0c71e70423e7c6b447344dc37916a
hungrated <zhang295415658@qq.com>
2017-12-07 13:55:33 +0800

    modify user pwdmod module

--- src/client/components/public/global-header-user-student.vue
+++ src/client/components/public/global-header-user-student.vue
@@ -6,12 +6,29 @@
       </template>
       <MenuGroup title="管 理">
         <MenuItem name="8-1"><span class="login-btn" @click="changeRoute('/user-center')">个人中心</span></MenuItem>
-        <MenuItem name="8-2"><span class="login-btn" @click="changeRoute('/user-center')">修改密码</span></MenuItem>
+        <MenuItem name="8-2"><span class="login-btn" @click="pwdMod = true">修改密码</span></MenuItem>
       </MenuGroup>
       <MenuGroup title="用 户">
         <MenuItem name="8-3"><span class="login-btn" @click="logout()">退出登录</span></MenuItem>
       </MenuGroup>
     </Submenu>
+    <Modal
+      v-model="pwdMod"
+      title="修改密码"
+      @on-ok="userPwdMod()">
+      <!--用户输入框-->
+      <div class="layout-login">
+        <i-input class="layout-login-input" type="password" v-model="password.currentPwd" placeholder="当前密码">
+          <Icon type="ios-locked-outline" slot="prepend"></Icon>
+        </i-input>
+        <i-input class="layout-login-input" type="password" v-model="password.newPwd" placeholder="新密码">
+          <Icon type="ios-locked" slot="prepend"></Icon>
+        </i-input>
+        <i-input class="layout-login-input" type="password" v-model="password.newPwdChk" placeholder="重复新密码">
+          <Icon type="ios-locked" slot="prepend"></Icon>
+        </i-input>
+      </div>
+    </Modal>
   </div>
 </template>
 
@@ -20,11 +37,36 @@
     name: 'global-header-user-student',
     props: ['name'],
     data () {
-      return {};
+      return {
+        pwdMod: false,
+        password: {
+          currentPwd: '',
+          newPwd: '',
+          newPwdChk: ''
+        }
+      };
     },
     methods: {
       changeRoute: function (path) {
         this.$router.push({path: path});
+      },
+      userPwdMod () {
+        if (this.password.newPwd !== this.password.newPwdChk) {
+          this.$Message.error('密码修改失败：新密码两次输入不一致');
+          return;
+        }
+        let _this = this;
+        this.$ajax.post('/api/user/pwdmod', {
+          username: JSON.parse(window.localStorage.user).school_id,
+          password: _this.password.currentPwd,
+          new_password: _this.password.newPwd
+        })
+          .then(function (res) {
+            _this.$Message.success(res.data.msg);
+          })
+          .catch(function (e) {
+            console.log(e);
+          });
       },
       logout () {
         let _this = this;
--- src/client/components/public/user-center-profile.vue
+++ src/client/components/public/user-center-profile.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="profile-container" class="profile-container">
     <div class="profile-avatar">
-      <img :src="profile.avatar">
+      <img style="width: 100%; border-radius: 5px;" :src="profile.avatar">
     </div>
     <div class="profile-info">
       <p class="profile-info-unit profile-info-name">
@@ -38,32 +38,48 @@
         {{profile.description}}
       </p>
     </div>
-    <Modal v-model="profileMng" title="编辑资料" @on-ok="profileSubmit()">
-      <div class="layout-login">
-        <p class="profile-info-unit profile-info-name">
-          <strong>{{profile.name}}</strong>
-        </p>
-        <!--性别-->
-        <p class="profile-info-unit">
-          <Icon type="university"></Icon>&emsp;{{profile.academy}} {{profile.grade}}级 {{profile.class_id}}班
-        </p>
-        <p class="profile-info-unit profile-info-sub">
-          <Icon type="card"></Icon>&emsp;
-          {{profile.school_id}}
-        </p>
-        <p class="profile-info-unit profile-info-sub">
-          <Icon type="person-stalker"></Icon>&emsp;
-          导 师：{{profile.supervisor}}
-        </p>
-        <i-input class="profile-edit-input" type="text" v-model="profile.birth_date" placeholder="出生日期">
-          <Icon type="ios-body-outline" slot="prepend"></Icon>
-        </i-input>
-        <i-input class="profile-edit-input" type="text" v-model="profile.phone_num" placeholder="电话号码">
-          <Icon type="ios-telephone-outline" slot="prepend"></Icon>
-        </i-input>
-        <i-input class="profile-edit-input" type="text" v-model="profile.description" placeholder="自 述">
-          <Icon type="ios-lightbulb-outline" slot="prepend"></Icon>
-        </i-input>
+    <Modal v-model="profileMng" title="编辑资料" @on-ok="profileSubmit()" @on-cancel="refreshData()">
+      <div class="profile-edit">
+        <div class="profile-edit-left">
+          <img style="width: 100%; border-radius: 5px;" :src="profile.avatar">
+        </div>
+        <div class="profile-edit-right">
+          <p class="profile-info-unit profile-info-name">
+            <strong>{{profile.name}}</strong>
+          </p>
+          <div style="margin-bottom: 10px">
+            <RadioGroup v-model="profile.sex">
+              <Radio label="男">
+                <Icon type="male"></Icon>
+                <span>男</span>
+              </Radio>
+              <Radio label="女">
+                <Icon type="female"></Icon>
+                <span>女</span>
+              </Radio>
+            </RadioGroup>
+          </div>
+          <p class="profile-info-unit">
+            <Icon type="university"></Icon>&emsp;{{profile.academy}} {{profile.grade}}级 {{profile.class_id}}班
+          </p>
+          <p class="profile-info-unit profile-info-sub">
+            <Icon type="card"></Icon>&emsp;
+            {{profile.school_id}}
+          </p>
+          <p class="profile-info-unit profile-info-sub">
+            <Icon type="person-stalker"></Icon>&emsp;
+            导 师：{{profile.supervisor}}
+          </p>
+          <i-input class="profile-edit-input" type="text" v-model="profile.birth_date" placeholder="出生日期">
+            <Icon type="ios-body-outline" slot="prepend"></Icon>
+          </i-input>
+          <i-input class="profile-edit-input" type="text" v-model="profile.phone_num" placeholder="电话号码">
+            <Icon type="ios-telephone-outline" slot="prepend"></Icon>
+          </i-input>
+          <i-input class="profile-edit-input" type="text" v-model="profile.description" placeholder="自 述">
+            <Icon type="ios-lightbulb-outline" slot="prepend"></Icon>
+          </i-input>
+        </div>
       </div>
     </Modal>
   </div>
@@ -75,7 +91,22 @@
     data () {
       return {
         profileMng: false,
-        profile: {}
+        profile: {
+//          school_id: null,
+//          avatar: null,
+//          name: null,
+//          sex: null,
+//          academy: null,
+//          class_id: null,
+//          grade: null,
+//          supervisor: null,
+//          birth_date: null,
+//          phone_num: null,
+//          description: null,
+//          created_at: null,
+//          updated_at: null,
+//          user_id: null
+        }
       };
     },
     methods: {
