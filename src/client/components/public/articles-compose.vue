9cca49dd682dbce23ee1b552ae5fe2b69a43fafe
hungrated <zhang295415658@qq.com>
2017-12-29 22:54:30 +0800

    correct mistakes

--- src/client/components/public/articles-compose.vue
+++ src/client/components/public/articles-compose.vue
@@ -13,7 +13,8 @@
           </div>
           <Input class="articles-compose-header-topic" v-model="editor.title" size="large" placeholder="文章标题"/>
           <ButtonGroup class="articles-compose-header-type" shape="circle" size="large">
-            <Button :type="(editType === 'richText') ? ('primary') : ('default')" @click="changeEditType('richText')" disabled>
+            <Button :type="(editType === 'richText') ? ('primary') : ('default')" @click="changeEditType('richText')"
+                    disabled>
               <Icon type="document-text" style="font-size: 17px"></Icon>
               富文本
             </Button>
@@ -59,7 +60,7 @@
   export default {
     name: 'articles-compose',
     components: {
-      mdEditor, rtEditor
+      mdEditor
     },
     data () {
       return {
