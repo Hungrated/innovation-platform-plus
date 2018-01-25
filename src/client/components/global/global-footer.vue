9e48a7894572e63582bac2eb50f37dfde1a6c0d9
hungrated <zhang295415658@qq.com>
2018-01-25 20:08:05 +0800

    modify styles

--- src/client/components/global/global-footer.vue
+++ src/client/components/global/global-footer.vue
@@ -19,7 +19,7 @@
       </i-col>
       <i-col span="6">
         <div class="g-footer sites">
-          <strong class="g-footer sites title">技术支持</strong>
+          <strong class="g-footer sites title">支 持</strong>
           <router-link class="sites-col" tag="span" v-for="site in sites.siteList2" :key="site.id" :to="site.route">
             {{site.text}}
           </router-link>
@@ -28,7 +28,7 @@
       <i-col span="6">
         <div class="g-footer sites">
           <strong class="g-footer sites title">其 他</strong>
-          <router-link class="sites-col" tag="span" v-for="site in sites.siteList2" :key="site.id" :to="site.route">
+          <router-link class="sites-col" tag="span" v-for="site in sites.siteList3" :key="site.id" :to="site.route">
             {{site.text}}
           </router-link>
         </div>
@@ -52,15 +52,18 @@
             {id: 3, text: '活动展示', route: '/articles?label=2'},
             {id: 4, text: '技术交流', route: '/articles?label=3'},
             {id: 5, text: '资源共享', route: '/resources'},
-            {id: 6, text: '公告栏', route: '/bulletin'},
+            // {id: 6, text: '公告栏', route: '/bulletin'},
             {id: 7, text: '讨论区', route: '/discussion'}
           ],
           siteList1: [
             {id: 8, text: '教师注册', route: '/index'}
           ],
           siteList2: [
-            {id: 9, text: '关于创新实践交流平台', route: '/index'},
+            {id: 9, text: '使用说明', route: '/index'},
             {id: 10, text: '联系我们（课程对接）', route: '/index'}
+          ],
+          siteList3: [
+            {id: 9, text: '关于创新实践课程交流平台', route: '/index'}
           ]
 
         }
