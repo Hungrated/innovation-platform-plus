c10727d186ccf1fde12232231fee6bc7d45e7d6f
hungrated <zhang295415658@qq.com>
2018-01-26 16:16:51 +0800

    correct mistakes

--- config/index.js
+++ config/index.js
@@ -58,10 +58,10 @@ module.exports = {
 
   build: {
     // Template for index.html
-    index: path.resolve(__dirname, '../dist/index.html'),
+    index: path.resolve(__dirname, '../src/server/public/index.html'),
 
     // Paths
-    assetsRoot: path.resolve(__dirname, '../dist'),
+    assetsRoot: path.resolve(__dirname, '../src/server/public'),
     assetsSubDirectory: 'static',
     assetsPublicPath: '/',
 
