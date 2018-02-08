fd17d191b4a0f09ea2a82b1f9ba711dfb09bd962
hungrated <zhang295415658@qq.com>
2018-02-08 20:41:21 +0800

    client-pc: modify article list style

--- src/client/components/main/articles-main.vue
+++ src/client/components/main/articles-main.vue
@@ -12,36 +12,166 @@
       </Menu>
     </div>
     <div class="g-articles header">
-      <article-view-carousel :articleList="articleList" :count="articleCount"/>
+      <carousel-3d class="g-container">
+        <slide :index="0">
+          <span class="g-carousel" v-if="!!carouselList[0]">
+            <span class="g-img">
+              <img :src="carouselList[0].cover">
+            </span>
+            <span class="g-title">
+              {{carouselList[0].title}}
+            </span>
+          </span>
+        </slide>
+        <slide :index="1">
+          <span class="g-carousel" v-if="!!carouselList[1]">
+            <span class="g-img">
+              <img :src="carouselList[1].cover">
+            </span>
+            <span class="g-title">
+              {{carouselList[1].title}}
+            </span>
+          </span>
+        </slide>
+        <slide :index="2">
+          <span class="g-carousel" v-if="!!carouselList[2]">
+            <span class="g-img">
+              <img :src="carouselList[2].cover">
+            </span>
+            <span class="g-title">
+              {{carouselList[2].title}}
+            </span>
+          </span>
+        </slide>
+        <slide :index="3">
+          <span class="g-carousel" v-if="!!carouselList[3]">
+            <span class="g-img">
+              <img :src="carouselList[3].cover || ''">
+            </span>
+            <span class="g-title">
+              {{carouselList[3].title || ''}}
+            </span>
+          </span>
+        </slide>
+        <slide :index="4">
+          <span class="g-carousel" v-if="!!carouselList[4]">
+            <span class="g-img">
+              <img :src="carouselList[4].cover|| ''">
+            </span>
+            <span class="g-title">
+              {{carouselList[4].title || ''}}
+            </span>
+          </span>
+        </slide>
+
+        <!--<slide v-for="item in carouselList" :key="item.blog_id" :index="item.index" v-model="carouselList">-->
+        <!--<span class="g-carousel">-->
+        <!--<span class="g-img">-->
+        <!--<img :src="carouselList[item.index].cover">-->
+        <!--</span>-->
+        <!--<span class="g-title">-->
+        <!--{{carouselList[item.index].title}}-->
+        <!--</span>-->
+        <!--</span>-->
+        <!--</slide>-->
+      </carousel-3d>
+      <!--<carousel-3d class="g-container" v-model="carouselList">-->
+      <!--<slide :index="0">-->
+      <!--<span class="g-carousel">-->
+      <!--<span class="g-img">-->
+      <!--<img :src="carouselList[0].cover">-->
+      <!--</span>-->
+      <!--<span class="g-title">-->
+      <!--{{carouselList[0].title}}-->
+      <!--</span>-->
+      <!--</span>-->
+      <!--</slide>-->
+      <!--<slide :index="1">-->
+      <!--<span class="g-carousel">-->
+      <!--<span class="g-img">-->
+      <!--&lt;!&ndash;<img :src="carouselList[1].cover">&ndash;&gt;-->
+      <!--</span>-->
+      <!--<span class="g-title">-->
+      <!--{{carouselList[1].title}}-->
+      <!--</span>-->
+      <!--</span>-->
+      <!--</slide>-->
+      <!--<slide :index="2">-->
+      <!--<span class="g-carousel">-->
+      <!--<span class="g-img">-->
+      <!--&lt;!&ndash;<img :src="carouselList[2].cover">&ndash;&gt;-->
+      <!--</span>-->
+      <!--<span class="g-title">-->
+      <!--{{carouselList[2].title}}-->
+      <!--</span>-->
+      <!--</span>-->
+      <!--</slide>-->
+      <!--<slide :index="3" :v-if="!!carouselList[3]">-->
+      <!--<span class="g-carousel">-->
+      <!--<span class="g-img">-->
+      <!--<img :src="carouselList[3].cover">-->
+      <!--</span>-->
+      <!--<span class="g-title">-->
+      <!--{{carouselList[3].title}}-->
+      <!--</span>-->
+      <!--</span>-->
+      <!--</slide>-->
+      <!--<slide :index="4" :v-if="!!carouselList[4]">-->
+      <!--<span class="g-carousel">-->
+      <!--<span class="g-img">-->
+      <!--<img :src="carouselList[4].cover">-->
+      <!--</span>-->
+      <!--<span class="g-title">-->
+      <!--{{carouselList[4].title}}-->
+      <!--</span>-->
+      <!--</span>-->
+      <!--</slide>-->
+      <!--</carousel-3d>-->
+      <!--<carousel-3d v-model="carouselList">-->
+      <!--<slide v-for="item in carouselList"-->
+      <!--:key="item.blog_id"-->
+      <!--:index="item.index">-->
+      <!--<div class="g-carousel">-->
+      <!--<div class="g-img">-->
+      <!--<img :src="item.cover">-->
+      <!--</div>-->
+      <!--<div class="g-title">-->
+      <!--{{item.title}}-->
+      <!--</div>-->
+      <!--</div>-->
+      <!--</slide>-->
+      <!--</carousel-3d>-->
+      <!--<article-view-carousel ref="carousel" :articleList="articleList"/>-->
 
       <!--<Card class="m-card" disHover>-->
-        <!--<div class="m-card container">-->
-          <!--<strong>文 章</strong>-->
-          <!--<Dropdown class="m-card container type">-->
-            <!--<Button type="primary" v-model="articleListLabel">-->
-              <!--{{articleListLabel}}&nbsp;-->
-              <!--<Icon type="arrow-down-b"></Icon>-->
-            <!--</Button>-->
-            <!--<DropdownMenu slot="list">-->
-              <!--<DropdownItem v-for="type in labelList"-->
-                            <!--:value="type.label"-->
-                            <!--:key="type.index">-->
-                <!--<span @click="changeLabel(type)">{{type.label}}</span>-->
-              <!--</DropdownItem>-->
-            <!--</DropdownMenu>-->
-          <!--</Dropdown>-->
-        <!--</div>-->
+      <!--<div class="m-card container">-->
+      <!--<strong>文 章</strong>-->
+      <!--<Dropdown class="m-card container type">-->
+      <!--<Button type="primary" v-model="articleListLabel">-->
+      <!--{{articleListLabel}}&nbsp;-->
+      <!--<Icon type="arrow-down-b"></Icon>-->
+      <!--</Button>-->
+      <!--<DropdownMenu slot="list">-->
+      <!--<DropdownItem v-for="type in labelList"-->
+      <!--:value="type.label"-->
+      <!--:key="type.index">-->
+      <!--<span @click="changeLabel(type)">{{type.label}}</span>-->
+      <!--</DropdownItem>-->
+      <!--</DropdownMenu>-->
+      <!--</Dropdown>-->
+      <!--</div>-->
       <!--</Card>-->
 
     </div>
     <div class="g-articles body">
-      <!--<article-view-list :articleList="articleList" :count="articleCount"/>-->
+      <article-view-list :articleList="articleList" :count="articleCount"/>
       <article-view-waterfall :articleList="articleList" :count="articleCount"/>
     </div>
   </div>
 </template>
 
 <script>
+  import { Carousel3d, Slide } from 'vue-carousel-3d';
   import articleViewCarousel from '../public/articles-view-carousel';
   import articleViewList from '../public/articles-view-list';
   import articleViewWaterfall from '../public/articles-view-waterfall';
@@ -79,7 +209,59 @@
           label: '',
           description: ''
         },
-        articleList: []
+        articleList: [],
+        carouselList: [
+          {
+            index: 0,
+            blog_id: '',
+            labels: null,
+            title: '',
+            description: '',
+            content: '',
+            cover: null,
+            publishTime: ''
+          },
+          {
+            index: 1,
+            blog_id: '',
+            labels: null,
+            title: '',
+            description: '',
+            content: '',
+            cover: null,
+            publishTime: ''
+          },
+          {
+            index: 2,
+            blog_id: '',
+            labels: null,
+            title: '',
+            description: '',
+            content: '',
+            cover: null,
+            publishTime: ''
+          },
+          {
+            index: 3,
+            blog_id: '',
+            labels: null,
+            title: '',
+            description: '',
+            content: '',
+            cover: null,
+            publishTime: ''
+          },
+          {
+            index: 4,
+            blog_id: '',
+            labels: null,
+            title: '',
+            description: '',
+            content: '',
+            cover: null,
+            publishTime: ''
+          }
+        ]
       };
     },
     methods: {
@@ -97,20 +279,40 @@
         })
           .then(function (res) {
             _this.articleList = res.data;
+            _this.carouselList = _this.getCarouselList(res.data);
             _this.articleCount = res.data.length;
           })
           .catch(function (e) {
             console.log(e);
           });
+      },
+      getCarouselList (list) {
+        let count = 0;
+        let carouselList = [];
+        for (let i = 0; i < list.length; i++) {
+          let item = list[i];
+          if (count >= 5) {
+            break;
+          }
+          if (item.cover) {
+            item.index = count++;
+            carouselList.push(item);
+          }
+        }
+        console.log(carouselList);
+        return carouselList;
       }
     },
     components: {
+      Carousel3d,
+      Slide,
       articleViewCarousel,
       articleViewList,
       articleViewWaterfall
     },
     mounted () {
       this.getArticleList();
+      console.log(this.carouselList);
     }
   };
 </script>
--- src/client/components/public/articles-view-carousel.vue
+++ src/client/components/public/articles-view-carousel.vue
@@ -1,25 +1,14 @@
 <template>
-  <carousel-3d>
-    <slide :index="0">
-      Slide 1 Content
-    </slide>
-    <slide :index="1">
-      Slide 2 Content
-    </slide>
-    <slide :index="2">
-      Slide 2 Content
-    </slide>
-    <slide :index="3">
-      Slide 2 Content
-    </slide>
-    <slide :index="4">
-      Slide 2 Content
-    </slide>
-    <slide :index="5">
-      Slide 2 Content
-    </slide>
-    <slide :index="6">
-      Slide 2 Content
+  <carousel-3d class="g-container" v-model="carouselList">
+    <slide v-for="item in carouselList" :key="item.blog_id" :index="item.index">
+      <span class="g-carousel">
+        <span class="g-img">
+          <img :src="carouselList[0].cover">
+        </span>
+        <span class="g-title">
+          {{carouselList[0].title}}
+        </span>
+      </span>
     </slide>
   </carousel-3d>
 </template>
@@ -28,21 +17,37 @@
   import { Carousel3d, Slide } from 'vue-carousel-3d';
 
   export default {
-    name: 'article-view-caoursel',
-    props: ['articleList', 'count'],
+    name: 'article-view-carousel',
+    props: ['articleList'],
     components: {
       Carousel3d,
       Slide
+    },
+    data () {
+      return {
+        carouselList: [{
+          index: 0,
+          blog_id: '',
+          labels: null,
+          title: '',
+          description: '',
+          content: '',
+          cover: null,
+          publishTime: ''
+        }]
+      };
     },
     methods: {
       revealDetails (index) {
         this.$router.push('/articles/details?index=' + index);
       }
+    },
+    mounted () {
     }
   };
 </script>
 
 <style scoped lang="scss">
-  @import "../../styles/article-list";
+  @import "../../styles/article-carousel";
 </style>
 
