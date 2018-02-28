1868957ffc413b487110ef2da3ba6adb53016b91
hungrated <zhang295415658@qq.com>
2018-02-28 17:47:25 +0800

    doc: update documentation

--- src/server/routes/route_labels.js
+++ src/server/routes/route_labels.js
@@ -8,7 +8,7 @@ const Label = db.Label;
 
 /**
  *
- * 提交标签
+ * 查询标签（用于编辑时获取添加）
  *
  * @api {post} /api/label/query label.query
  * @apiName labelQuery
@@ -16,7 +16,38 @@ const Label = db.Label;
  * @apiVersion 3.0.0
  * @apiPermission user
  *
- * @apiSuccess {JSON} data Response data.
+ * @apiDescription 查询类型标签。
+ *
+ * @apiParam {String} type 标签类型 可选： blog|file|both
+ *
+ * @apiParamExample {json} 请求示例
+ * {
+ *     "name": "JavaScript",
+ *     "category": "blog",
+ *     "adder_id": 40429
+ * }
+ *
+ * @apiSuccess {Array} labelArr 标签详细信息
+ * @apiSuccessExample {json} 成功返回示例
+ * HTTP/1.1 200 OK
+ * [
+ *     {
+ *         "label_id": 1,
+ *         "name": "JavaScript",
+ *         "category": "both",
+ *         "created_at": "2018-02-26T10:41:18.000Z",
+ *         "updated_at": "2018-02-26T10:41:18.000Z",
+ *         "adder_id": 40429
+ *     },
+ *     {
+ *         "label_id": 2,
+ *         "name": "TypeScript",
+ *         "category": "both",
+ *         "created_at": "2018-02-27T14:50:28.000Z",
+ *         "updated_at": "2018-02-27T14:50:28.000Z",
+ *         "adder_id": 40429
+ *     }
+ * ]
  *
  */
 router.post('/query', function (req, res) {
@@ -45,16 +76,37 @@ router.post('/query', function (req, res) {
 
 /**
  *
- * 提交标签
+ * 新增标签
  *
- * @api {post} /api/label/query label.query
- * @apiName labelQuery
+ * @api {post} /api/label/submit label.submit
+ * @apiName labelSubmit
  * @apiGroup Label
- * @apiVersion 2.5.0
+ * @apiVersion 3.0.0
  * @apiPermission user.teacher
  *
- * @apiSuccess {JSON} data Response data.
+ * @apiDescription 教师新增类型标签。
  *
+ * @apiParam {String} name 标签名
+ * @apiParam {String} category 标签类型 可选： blog|file|both
+ * @apiParam {Number} adder_id 添加者编号
+ *
+ * @apiParamExample {json} 请求示例
+ * {
+ *     "name": "JavaScript",
+ *     "category": "blog",
+ *     "adder_id": 40429
+ * }
+ *
+ * @apiSuccess {Number} status 状态代码
+ * @apiSuccess {String} msg 反馈信息
+ * @apiSuccess {Number} label_id 标签编号
+ * @apiSuccessExample {json} 成功返回示例
+ * HTTP/1.1 200 OK
+ * {
+ *     "status": 8400,
+ *     "msg": "标签创建成功",
+ *     "label_id": 12
+ * }
  */
 router.post('/submit', function (req, res) {
   Label.create({
@@ -77,6 +129,38 @@ router.post('/submit', function (req, res) {
     });
 });
 
+/**
+ *
+ * 修改标签
+ *
+ * @api {post} /api/label/mod label.mod
+ * @apiName labelMod
+ * @apiGroup Label
+ * @apiVersion 3.1.0
+ * @apiPermission user.teacher
+ *
+ * @apiDescription 教师修改类型标签。
+ *
+ * @apiParam {String} type 标签类型 可选： blog|file
+ * @apiParam {String} id 信息标识
+ * @apiParam {String} labels 新标签列表
+ *
+ * @apiParamExample {json} 请求示例
+ * {
+ *     "type": "file",
+ *     "id": "fila507d7",
+ *     "labels": "4,5,2"
+ * }
+ *
+ * @apiSuccess {Number} status 状态代码
+ * @apiSuccess {String} msg 反馈信息
+ * @apiSuccessExample {json} 成功返回示例
+ * HTTP/1.1 200 OK
+ * {
+ *     "status": 8500,
+ *     "msg": "标签修改成功",
+ * }
+ */
 router.post('/mod', function (req, res) {
   let database = req.body.type === 'blog'
     ? db.Blog : db.File;
