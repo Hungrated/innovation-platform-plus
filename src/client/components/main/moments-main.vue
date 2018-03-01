1631433bc9b097674aadde4f7aafbbaa939ab39d
hungrated <zhang295415658@qq.com>
2018-03-01 12:18:14 +0800

    client-pc: modify moments & fix backend bugs

--- src/client/components/main/moments-main.vue
+++ src/client/components/main/moments-main.vue
@@ -1,47 +1,45 @@
 <template>
   <div id="moments" class="g-moments">
     <Timeline>
-      <Card disHover>
-        <span slot="title">
-          <span class="g-moments header">
-            <strong>动 态</strong>
-          </span>
-        </span>
-        <p v-if="moments.length === 0" style="text-align: center">暂无动态哦</p>
-        <div class="g-moments container" v-else>
-          <TimelineItem v-for="moment in moments" :key="moment.moment_id">
-            <div v-if="moment.type === 'planmod'" class="m-moment">
+      <p v-if="moments.length === 0" style="text-align: center">暂无动态哦</p>
+      <div class="m-container" v-else>
+        <TimelineItem v-for="moment in moments" :key="moment.moment_id">
+          <!--<div v-if="moment.type === 'planmod'" class="m-moment">-->
+          <!--<span class="m-moment time">{{ getTime(moment.created_at) }}</span>-->
+          <!--<div>-->
+          <!--<Icon type="ios-lightbulb"></Icon>&nbsp;&nbsp;-->
+          <!--<strong>{{moment.profile.name}}</strong>&nbsp;更新了计划：-->
+          <!--<span>{{moment.desc}}</span>-->
+          <!--</div>-->
+          <!--</div>-->
+
+          <div v-if="moment.type === 'article'" class="m-moment g-unit">
+            <div class="g-unit left">
               <span class="m-moment time">{{ getTime(moment.created_at) }}</span>
-              <div>
-                <Icon type="ios-lightbulb"></Icon>&nbsp;&nbsp;
-                <strong>{{moment.profile.name}}</strong>&nbsp;更新了计划：
-                <span>{{moment.desc}}</span>
-              </div>
             </div>
-            <div v-if="moment.type === 'article'" class="m-moment">
-              <span class="m-moment time">{{ getTime(moment.created_at) }}</span>
-              <div>
+            <Card disHover class="g-unit right">
+              <span slot="title">
                 <Icon type="document-text"></Icon>&nbsp;&nbsp;
                 <strong>{{moment.profile.name}}</strong>&nbsp;发布了文章：
                 <span>{{moment.desc}}</span>
-                <a :href="'/#' + moment.href">查 看</a>
-              </div>
-            </div>
-            <div v-if="moment.type === 'resource'" class="m-moment">
-              <span class="m-moment time">{{ getTime(moment.created_at) }}</span>
-              <div>
-                <Icon type="android-folder-open"></Icon>&nbsp;
-                <strong>{{moment.profile.name}}</strong>&nbsp;上传了资源：
-                <span>{{moment.desc}}</span>
-                <a :href="moment.href">下 载</a>
-              </div>
-            </div>
-          </TimelineItem>
-        </div>
-      </Card>
+                <a class="m-details" :href="'/#' + moment.extras.href">查 看</a>
+              </span>
+            </Card>
+          </div>
+
+          <!--<div v-if="moment.type === 'resource'" class="m-moment">-->
+          <!--<span class="m-moment time">{{ getTime(moment.created_at) }}</span>-->
+          <!--<div>-->
+          <!--<Icon type="android-folder-open"></Icon>&nbsp;-->
+          <!--<strong>{{moment.profile.name}}</strong>&nbsp;上传了资源：-->
+          <!--<span>{{moment.desc}}</span>-->
+          <!--<a :href="moment.extras.href">下 载</a>-->
+          <!--</div>-->
+          <!--</div>-->
+        </TimelineItem>
+      </div>
     </Timeline>
   </div>
-
 </template>
 
 <script>
@@ -63,6 +61,9 @@
         this.$ajax.get('/api/moment/fetch?type=all&limit=' + this.limit)
           .then(function (res) {
             _this.moments = res.data;
+            for (let i = 0; i < res.data.length; i++) {
+              _this.moments[i].extras = JSON.parse(res.data[i].extras);
+            }
           })
           .catch(function (e) {
             console.log(e);
